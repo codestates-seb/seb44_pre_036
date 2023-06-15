c980bfec7b1c656e5054a89e14b2f3b6cba43ebc
jasmine0714 <woooooooow22@gmail.com>
2023-06-15 20:33:30 +0900

    💄 회원가입 form, input 생성

---
+++ client/src/pages/SignUp/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const StyledInput = styled.input`
+    height: 28.5px;
+    width: 260px;
+    border: 1px solid #b8afaf;
+    border-radius: 3px;
+`
