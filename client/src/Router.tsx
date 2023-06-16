01723adf226d823bc595c794c924f3c92ef89f3e
jasmine0714 <woooooooow22@gmail.com>
2023-06-16 21:10:52 +0900

    ⏪ 주석 해제

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/views/Home';
 import MyPage from './pages/MyPage/views/MyPage';
