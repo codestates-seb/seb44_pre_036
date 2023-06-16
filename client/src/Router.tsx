3a81d487bd6e832e37b53547b958ea1574a36879
jasmine0714 <woooooooow22@gmail.com>
2023-06-16 21:08:54 +0900

    🚚 Register에서 SignUp으로 변경

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/views/Home';
 import MyPage from './pages/MyPage/views/MyPage';
 import Board from './pages/Board/views/Board';
 import Login from './pages/Login/views/Login';
-import Register from './pages/Register/views/Register';
 import Ask from './pages/Ask/views/Ask';
 import SignUp from './pages/SignUp/views/SignUp';
 
@@ -17,7 +16,6 @@ function Router() {
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/board" element={<Board />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/register" element={<Register />} /> */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/ask" element={<Ask />} />
         </Routes>
