c98c3cb814134d1a84ffd23f0927b102e4fabc11
Mooobi <antod123@naver.com>
2023-06-19 00:53:00 +0900

    🩹 register 라우트 삭제, detail 라우트 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/views/Home';
 import MyPage from './pages/MyPage/views/MyPage';
 import Board from './pages/Board/views/Board';
 import Login from './pages/Login/views/Login';
-import Register from './pages/Register/views/Register';
 import Ask from './pages/Ask/views/Ask';
+import SignUp from './pages/SignUp/views/SignUp';
+import Detail from './pages/Detail/views/Detail';
 
 function Router() {
   return (
@@ -16,8 +16,9 @@ function Router() {
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/board" element={<Board />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/ask" element={<Ask />} />
+          <Route path="/detail" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </div>
