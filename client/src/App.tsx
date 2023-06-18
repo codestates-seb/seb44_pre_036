559cf0659f50ae1cf93a57625c69686814f53b96
jasmine0714 <woooooooow22@gmail.com>
2023-06-18 14:54:42 +0900

    ✨ 자동 로그인 기능 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import Router from './Router';
+import useGetMe from './common/utils/customHook/useGetMe';
 
 function App() {
+  const getMe = useGetMe();
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userInfo = await getMe();
+      if (!userInfo) {
+        return;
+      }
+      console.log(userInfo);
+    };
+
+    fetchUserInfo();
+  }, []);
+
   return <Router />;
 }
 
