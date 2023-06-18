8702213ec9a380e4730a21ca87c496b8341d3b4c
Mooobi <antod123@naver.com>
2023-06-19 00:40:56 +0900

    💄 global css 추가

---
+++ client/src/common/style.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;
