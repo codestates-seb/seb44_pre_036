ada71b0e9da8e7c6e2e0ef4d4228b4f4a1553d2e
jasmine0714 <woooooooow22@gmail.com>
2023-06-18 15:38:46 +0900

    ⚡️ access token 만료 또는 refresh token 만료 시 로직 추가

--- client/src/common/utils/customHook/useGetMe.tsx
+++ client/src/common/utils/customHook/useGetMe.tsx
@@ -15,10 +15,24 @@ function useGetMe(): () => Promise<IUserInfo | null> {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
+        // 이 옵션은 쿠키를 서버와 주고받을 수 있게 해준다.
+        withCredentials: true,
       });
+      // accessToken이 만료되었을 경우 재발급
+      if (response.data.accessToken) {
+        localStorage.removeItem(ACCESS_TOKEN);
+        localStorage.setItem(ACCESS_TOKEN, response.data.accessToken);
+      }
+
       return response.data;
-    } catch (error) {
-      console.log(error);
+    } catch (error: Error | any) {
+      // refresh token 만료 시 로그아웃 처리
+      if (error.response.status === 401) {
+        localStorage.removeItem(ACCESS_TOKEN);
+      } else {
+        console.error(error);
+      }
+
       return null;
     }
   };
