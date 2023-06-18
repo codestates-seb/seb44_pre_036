7c1aa5a8d641f60d83ac93d2ef628cabf61eef36
Mooobi <antod123@naver.com>
2023-06-19 00:55:19 +0900

    ⚡️ reactquery 및 globalstyle 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import GlobalStyle from './common/style.ts';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
