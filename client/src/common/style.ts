b975a943f3f695b9b7781d656940892910e19078
jasmine0714 <woooooooow22@gmail.com>
2023-06-18 14:05:52 +0900

    🚚 회원가입 시 사용했던 컴포넌트 이메일 로그인에 재사용하기 위해 common으로 이동

--- client/src/common/style.ts
+++ client/src/common/style.ts
@@ -4,3 +4,41 @@ export const StyledLabel = styled.label`
   font-size: 15px;
   font-weight: 700;
 `;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #b8afaf;
+  border-radius: 7px;
+  width: 300px;
+`;
+
+export const StyledInput = styled.input`
+  height: 28.5px;
+  width: 260px;
+  border: 1px solid #b8afaf;
+  border-radius: 3px;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 260px;
+`;
+
+export const Text = styled.p`
+  font-size: 13px;
+  color: gray;
+`;
+
+export const UserInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  margin-top: 20px;
+`;
