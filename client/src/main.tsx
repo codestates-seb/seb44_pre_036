f13a2bb1b0fde61e9745457d0bdec5f07392e124
jasmine0714 <woooooooow22@gmail.com>
2023-06-18 16:38:17 +0900

    🥅 Redux store 사용을 위해 Provider 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from '../src/common/store/RootStore';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 );
