050826a770352d33d4b6cf4b624fdc026b03ae1e
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-06-19 19:45:16 +0900

    🎨Background-color 추가

--- client/src/common/GNB/components/DropdownStyles.ts
+++ client/src/common/GNB/components/DropdownStyles.ts
@@ -11,6 +11,7 @@ export const Container = styled.div`
   border-bottom: 1px solid #d5d5d5;
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
     0 2px 8px hsla(0, 0%, 0%, 0.05);
+  background-color: #ffffff;
 `;
 export const InBox = styled.div`
   width: 100%;
