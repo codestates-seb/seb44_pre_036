0e650aaeb50f0489d2b9f18373a3fa6ffe251e40
jasmine0714 <woooooooow22@gmail.com>
2023-06-19 22:34:16 +0900

    👽️ 계정 정보 전달하는 URL enum으로 관리

--- client/src/common/type.ts
+++ client/src/common/type.ts
@@ -7,3 +7,8 @@ export type UserInfoFieldSignUp = 'name' | 'email' | 'password';
 export type UserInfoField = 'email' | 'password';
 
 export type Token = 'accessToken' | 'refreshToken';
+
+export const enum MembershipUrl {
+  signUp = 'https://6564-220-127-158-194.ngrok-free.app/members',
+  Login = 'https://6564-220-127-158-194.ngrok-free.app/auth/login',
+}
