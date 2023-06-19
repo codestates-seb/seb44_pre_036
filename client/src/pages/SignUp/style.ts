51251182247e3667517703a96059bc0e8e95b42d
jasmine0714 <woooooooow22@gmail.com>
2023-06-19 16:18:35 +0900

    💄 회원가입/로그인에 사용할 버튼 컴포넌트 생성

---
+++ client/src/pages/SignUp/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { BlueButton } from '../../common/style';
+import { BlueButtonProps } from '../SignUp/type';
+
+export const BlueButtonMembership = styled(BlueButton)<BlueButtonProps>`
+  width: 260px;
+  margin: 20px;
+`;
