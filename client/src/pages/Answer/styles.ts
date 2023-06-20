31ac77efd444ec04dec75dd2bd34b8adad506386
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-06-20 15:11:01 +0900

    🎨Answer style 설정

---
+++ client/src/pages/Answer/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const AnswerForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+  border-radius: 8px;
+  max-width: 600px;
+  margin: 0 auto;
+  box-shadow: 12px 24px 24px #ededed;
+`;
