724993e2308df7af082bb14b9fa7c63b48e5def4
minit88 <chymh32@gmail.com>
2023-06-20 09:02:06 +0900

    :card_file_box: 답변 매퍼 수정

--- server/src/main/java/seb44pre036/qna/answer/mapper/AnswerMapper.java
+++ server/src/main/java/seb44pre036/qna/answer/mapper/AnswerMapper.java
@@ -9,31 +9,33 @@ import seb44pre036.qna.member.dto.MemberDto;
 import seb44pre036.qna.member.entity.Member;
 import seb44pre036.qna.member.service.MemberService;
 import seb44pre036.qna.question.entity.Question;
+import seb44pre036.qna.question.service.QuestionService;
 
 
 @Mapper(componentModel = "spring")
 public interface AnswerMapper  {
 
-    default Answer answerPostDtoToAnswer(MemberService memberService, AnswerService answerService, AnswerDto.Post answerPostDto){
+    default Answer answerPostDtoToAnswer(MemberService memberService, AnswerService answerService,QuestionService questionService, AnswerDto.Post answerPostDto){
         Answer answer = new Answer();
         answer.setContent(answerPostDto.getContent());
-        answer.setAccepted(false);
-        //answer.setQuestion();
+
         answer.setMember(memberService.findVerifiedMember(answerPostDto.getMemberId()));
 
-        //수정 필요
-        answer.setQuestion(new Question());
+        //수정 필요 ( question post가 등록되지 않아 builder 사용으로 질문 객체 직접 주입)
+        //answer.setQuestion(questionService.findQuestion(answerPostDto.getQuestionId()));
+        Question question = Question.builder().content("dasda").title("dada").viewCount(6).build();
+        answer.setQuestion(question);
+
 
         return answer;
     }
 
 
-    default Answer answerPatchDtoToAnswer(MemberService memberService, AnswerService answerService, AnswerDto.Patch answerPatchDto){
+    default Answer answerPatchDtoToAnswer(MemberService memberService, AnswerService answerService,  AnswerDto.Patch answerPatchDto){
         Answer answer = new Answer();
 
         answer.setAnswerId(answerPatchDto.getAnswerId());
         answer.setContent(answerPatchDto.getContent());
-        answer.setAccepted(answerPatchDto.isAccepted());
 
         return answer;
     }
@@ -41,13 +43,16 @@ public interface AnswerMapper  {
     default AnswerDto.Response answerToAnswerDtoResponse(Answer answer){
         AnswerDto.Response answerDtoResponse = new AnswerDto.Response();
         answerDtoResponse.setAnswerId(answer.getAnswerId());
-        answerDtoResponse.setAccepted(answer.isAccepted());
+        answerDtoResponse.setAccepted(answerDtoResponse.isAccepted());
         answerDtoResponse.setContent(answer.getContent());
         answerDtoResponse.setCreatedAt(answer.getCreatedAt());
         answerDtoResponse.setUpdatedAt(answer.getUpdatedAt());
 
         answerDtoResponse.setMemberId(answer.getMember().getMemberId());
+
+        //질문 쪽 post가 되지 않아 수동 설정
         answerDtoResponse.setQuestionId(answer.getQuestion().getQuestionId());
+        //answerDtoResponse.setQuestionId(1);
 
         return answerDtoResponse;
     }
