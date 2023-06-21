0d9799d7a5b116ee44c1e4e14f66ab1ec7e5b9dc
jasmine0714 <woooooooow22@gmail.com>
2023-06-22 06:25:07 +0900

    ✨ access token local storage에 저장 시 클라이언트 자체 암호화

--- client/src/common/utils/customHook/useGetMe.tsx
+++ client/src/common/utils/customHook/useGetMe.tsx
@@ -1,19 +1,35 @@
 import { UseQueryResult, useQuery } from 'react-query';
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 import { ACCESS_TOKEN } from '../constants';
 import { useDispatch } from 'react-redux';
 import { createUserInfo } from '../../store/UserInfoStore';
 import { IUserInfo } from '../../model/UserInfo';
 import { MembershipUrl } from '../enum';
 
+const secretKey = import.meta.env.VITE_SECRET_KEY;
+
+// Encrypt the access token
+const encryptToken = (tokenToEncrypt: string) => {
+  return CryptoJS.AES.encrypt(tokenToEncrypt, secretKey).toString();
+};
+
+// Decrypt the access token
+const decryptToken = (encryptedToken: string) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedToken, secretKey);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+
 function useGetMe(): UseQueryResult<IUserInfo | null> {
   const dispatch = useDispatch();
   const getMe = async (): Promise<IUserInfo | null | undefined> => {
-    const accessToken = localStorage.getItem(ACCESS_TOKEN);
-    console.log('localstorage에서 받아온 accessToken', accessToken);
-    if (!accessToken) {
+    const encryptedAccessToken = localStorage.getItem(ACCESS_TOKEN);
+
+    if (!encryptedAccessToken) {
       return null;
     }
+
+    const accessToken = decryptToken(encryptedAccessToken);
 
     const headers = {
       'ngrok-skip-browser-warning': 'true',
@@ -28,23 +44,23 @@ function useGetMe(): UseQueryResult<IUserInfo | null> {
 
       const userData: IUserInfo = response.data;
 
-      dispatch(createUserInfo(userData));
-
-      if (userData === null) {
+      if (!userData || !response?.headers.authorization) {
         return null;
       }
+
+      dispatch(createUserInfo(userData));
 
       //   accessToken이 만료되었을 경우 재발급
       const newAccessToken = response.headers.authorization.split(' ')[1];
       if (newAccessToken) {
         localStorage.removeItem(ACCESS_TOKEN);
-        localStorage.setItem(ACCESS_TOKEN, newAccessToken);
+        localStorage.setItem(ACCESS_TOKEN, encryptToken(newAccessToken));
       }
 
       return userData;
     } catch (error: any) {
       // refresh token 만료 시 로그아웃 처리
-      if (error.response.status === 401) {
+      if (error?.response?.status === 401) {
         localStorage.removeItem(ACCESS_TOKEN);
       } else {
         console.error(error);
