f76a1824e7e1a547ab46ecce680bcfacccb566ff
Mooobi <antod123@naver.com>
2023-06-21 17:36:57 +0900

    💄 Edit 페이지 UI 구현

--- client/src/common/store/RootStore.tsx
+++ client/src/common/store/RootStore.tsx
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userInfo } from './UserInfoStore';
 import { filterSlice } from '../../pages/Board/store/FilterStore';
+import { previewSlice } from '../../pages/Edit/store/InputStore';
 
 const rootReducer = combineReducers({
   userInfo: userInfo.reducer,
   filter: filterSlice.reducer,
+  preview: previewSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ client/src/pages/Edit/components/TitleInput.tsx
@@ -0,0 +1,13 @@
+import { item } from '../../Board/type';
+import { TitleInputBox, TitleInputContainer } from '../style';
+
+const TitleInput = ({ item }: { item: item }) => {
+  return (
+    <TitleInputContainer>
+      <h4>Title</h4>
+      <TitleInputBox type="text" value={item.title} />
+    </TitleInputContainer>
+  );
+};
+
+export default TitleInput;
