d65906b9a10c31a1081a2fd9277469405f7c9d0b
jasmine0714 <woooooooow22@gmail.com>
2023-06-21 20:32:10 +0900

    ✨ 로그아웃 및 회원 탈퇴 기능 구현

--- client/src/pages/Login/components/LoginForm.tsx
+++ client/src/pages/Login/components/LoginForm.tsx
@@ -16,7 +16,6 @@ import {
   email,
   password,
   ACCESS_TOKEN,
-  REFRESH_TOKEN,
   PASSWORD_MIN_LENGTH,
   EMAIL_REGEX,
   PASSWORD_REGEX,
@@ -28,10 +27,13 @@ import {
 } from '../../../common/utils/constants';
 import ConfirmButton from '../../SignUp/components/ConfirmButton';
 import { MembershipUrl } from '../../../common/utils/enum';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../common/store/RootStore';
 
 const postData = async (data: IUserInfoLogin) => {
   const response = await axios.post(MembershipUrl.Login, data);
-  console.log('준기님께 계정 정보 전달 후 받아온 데이터', response.headers);
+  console.log('2 준기님께 계정 정보 전달 후 받아온 데이터', response.headers);
   return response.headers;
 };
 
@@ -47,20 +49,25 @@ function LoginForm() {
   const { refetch: refetchGetMe } = useGetMe();
 
   const mutation = useMutation(postData, {
-    onSuccess: async (data) => {
-      if (!data) {
+    onSuccess: async (header) => {
+      if (!header) {
         return;
       }
-      const accessToken = data.authorization.split(' ')[1];
+      const accessToken: string = header.authorization.split(' ')[1];
+      console.log(
+        '3 준기님께 계정 정보 전달 후 받아온 accessToken',
+        accessToken,
+      );
 
       localStorage.removeItem(ACCESS_TOKEN);
       localStorage.setItem(ACCESS_TOKEN, accessToken);
 
-      const { data: userData } = await refetchGetMe();
-      console.log(userData);
+      await refetchGetMe();
+
+      navigate('/');
     },
     onError: (error) => {
-      console.error(error);
+      console.log(error);
       // TODO: 에러 처리
       // 이메일 형식이 잘못됨 (400) -> The email is not a valid email address.
       // 비밀번호가 잘못됨 (401) -> The email or password is incorrect.
@@ -70,45 +77,61 @@ function LoginForm() {
   });
 
   const onSubmit = async (userData: IUserInfoLogin) => {
-    console.log(userData);
+    console.log('1 유저데이터', userData);
     setIsClicked(true);
 
     await mutation.mutateAsync(userData);
   };
 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem(ACCESS_TOKEN);
+    navigate('/');
+  };
+
+  const memberId = useSelector((state: RootState) => state.userInfo.memberId);
+  const handleWithdrawal = async () => {
+    const response = await axios.delete(
+      MembershipUrl.Withdrawal + `/${memberId}`,
+    );
+    console.log(response);
+  };
+
   return (
-    <StyledForm onSubmit={handleSubmit(onSubmit)}>
-      <UserInfoWrapper>
-        <UserInfoLabel label={'Email'} />
-        <StyledInput
-          {...register(email, {
-            required: WARNING_MESSAGE_EMAIL_EMPTY,
-            pattern: {
-              value: EMAIL_REGEX,
-              message: `${watch(email)} is not a valid email address`,
-            },
-          })}
-        />
-        {errors?.email?.message === WARNING_MESSAGE_EMAIL_EMPTY ||
-        (isClicked && typeof errors?.email?.message === 'string') ? (
-          <ErrorMsg>{errors.email.message}</ErrorMsg>
-        ) : null}
-      </UserInfoWrapper>
-      <UserInfoWrapper>
-        <UserInfoLabel label={'Password'} />
-        <StyledInput
-          type="password"
-          {...register(password, {
-            required: WARNING_MESSAGE_PASSWORD_EMPTY,
-            minLength: {
-              value: PASSWORD_MIN_LENGTH,
-              message: `Must contain at least ${
-                PASSWORD_MIN_LENGTH - (watch(password)?.length || 0)
-              } more characters.`,
-            },
-            pattern: {
-              value: PASSWORD_REGEX,
-              message: `${WARNING_MESSAGE_PASSWORD_WEAK}: 
+    <>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <UserInfoWrapper>
+          <UserInfoLabel label={'Email'} />
+          <StyledInput
+            {...register(email, {
+              required: WARNING_MESSAGE_EMAIL_EMPTY,
+              pattern: {
+                value: EMAIL_REGEX,
+                message: `${watch(email)} is not a valid email address`,
+              },
+            })}
+          />
+          {errors?.email?.message === WARNING_MESSAGE_EMAIL_EMPTY ||
+          (isClicked && typeof errors?.email?.message === 'string') ? (
+            <ErrorMsg>{errors.email.message}</ErrorMsg>
+          ) : null}
+        </UserInfoWrapper>
+        <UserInfoWrapper>
+          <UserInfoLabel label={'Password'} />
+          <StyledInput
+            type="password"
+            {...register(password, {
+              required: WARNING_MESSAGE_PASSWORD_EMPTY,
+              minLength: {
+                value: PASSWORD_MIN_LENGTH,
+                message: `Must contain at least ${
+                  PASSWORD_MIN_LENGTH - (watch(password)?.length || 0)
+                } more characters.`,
+              },
+              pattern: {
+                value: PASSWORD_REGEX,
+                message: `${WARNING_MESSAGE_PASSWORD_WEAK}: 
                   ${
                     watch(password) &&
                     PASSWORD_REGEX_NO_LETTERS.test(watch(password))
@@ -121,20 +144,27 @@ function LoginForm() {
                       ? 'numbers'
                       : ''
                   }`,
-            },
-          })}
+              },
+            })}
+          />
+          {errors?.password?.message === WARNING_MESSAGE_PASSWORD_EMPTY ||
+          (isClicked && typeof errors?.password?.message === 'string') ? (
+            <ErrorMsg>{errors.password.message}</ErrorMsg>
+          ) : null}
+        </UserInfoWrapper>
+        <ConfirmButton
+          type="submit"
+          setIsClicked={setIsClicked}
+          buttontext={'Log in'}
         />
-        {errors?.password?.message === WARNING_MESSAGE_PASSWORD_EMPTY ||
-        (isClicked && typeof errors?.password?.message === 'string') ? (
-          <ErrorMsg>{errors.password.message}</ErrorMsg>
-        ) : null}
-      </UserInfoWrapper>
-      <ConfirmButton
-        type="submit"
-        setIsClicked={setIsClicked}
-        buttontext={'Log in'}
-      />
-    </StyledForm>
+        <button onClick={handleLogout}>log out</button>
+      </StyledForm>
+      <section>
+        <h1>회원 탈퇴</h1>
+
+        <button onClick={handleWithdrawal}>withdraw</button>
+      </section>
+    </>
   );
 }
 export default LoginForm;
