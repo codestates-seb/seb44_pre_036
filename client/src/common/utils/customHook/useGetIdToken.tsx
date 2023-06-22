385d980cce8ec1e77769183c2def4d432c1e257c
jasmine0714 <woooooooow22@gmail.com>
2023-06-22 22:02:08 +0900

    🔐 Client_Id를 .env에 보관

--- client/src/common/utils/customHook/useGetIdToken.tsx
+++ client/src/common/utils/customHook/useGetIdToken.tsx
@@ -7,16 +7,13 @@ function useGetIdToken(): GetIdTokenType {
 
   useEffect(() => {
     const handleCredentialResponse = (response: any) => {
-      // Handle the response after the user signs in with Google
-      console.log('응답', response); // ID token can be accessed using response.credential
-      // You can pass the ID token to your backend for further processing
+      console.log('응답', response);
       setIdToken(response.credential);
     };
 
     const renderButton = () => {
       window.google.accounts.id.initialize({
-        client_id:
-          '1082895007031-pbcem7t8pavstntj3biuvie6n35in5d9.apps.googleusercontent.com', // Replace with your client ID
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
         callback: handleCredentialResponse,
       });
 
@@ -42,7 +39,6 @@ function useGetIdToken(): GetIdTokenType {
     loadGoogleScript();
 
     return () => {
-      // Clean up: remove the dynamically added script element
       const script = document.querySelector(
         'script[src="https://accounts.google.com/gsi/client"]',
       );
