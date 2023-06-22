6e8c15f7bda4f1e51bbd8539e2aade62a8819e6a
jasmine0714 <woooooooow22@gmail.com>
2023-06-22 21:28:46 +0900

    💄 stackoverflow logo와 구글 로그인 버튼 구현

--- client/src/pages/Login/views/Login.tsx
+++ client/src/pages/Login/views/Login.tsx
@@ -1,9 +1,13 @@
 import LoginForm from './LoginForm';
 import { LoginPageContainer } from '../style';
+import GoogleLoginBtn from '../../../common/components/GoogleLoginBtn';
+import { StackoverflowLogo } from '../../../common/style';
 
 function Login() {
   return (
     <LoginPageContainer>
+      <StackoverflowLogo />
+      <GoogleLoginBtn />
       <LoginForm />
     </LoginPageContainer>
   );
