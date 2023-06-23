e703c818935184892231ff4a048d7b9db0869e62
RedJunKi <bing8821@naver.com>
2023-06-23 09:31:43 +0900

    :necktie: 회원가입시 토큰발급, OAuth2 로직 구현중

--- server/src/main/generated/seb44pre036/qna/question/mapper/QuestionMapperImpl.java
+++ server/src/main/generated/seb44pre036/qna/question/mapper/QuestionMapperImpl.java
@@ -2,13 +2,12 @@ package seb44pre036.qna.question.mapper;
 
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
-import seb44pre036.qna.member.entity.Member;
 import seb44pre036.qna.question.dto.QuestionDto;
 import seb44pre036.qna.question.entity.Question;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-21T15:14:29+0900",
+    date = "2023-06-23T09:06:59+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
 )
 @Component
@@ -22,23 +21,10 @@ public class QuestionMapperImpl implements QuestionMapper {
 
         Question.QuestionBuilder question = Question.builder();
 
-        question.member( patchToMember( requestBody ) );
         question.questionId( requestBody.getQuestionId() );
         question.title( requestBody.getTitle() );
         question.content( requestBody.getContent() );
 
         return question.build();
-    }
-
-    protected Member patchToMember(QuestionDto.Patch patch) {
-        if ( patch == null ) {
-            return null;
-        }
-
-        Member member = new Member();
-
-        member.setMemberId( patch.getMemberId() );
-
-        return member;
     }
 }
