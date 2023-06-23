b0681e938da95ead1ea5922b23fd872de1d67212
minit88 <chymh32@gmail.com>
2023-06-23 09:04:04 +0900

    :sparkles:  addAnswer추가

--- server/src/main/java/seb44pre036/qna/question/entity/Question.java
+++ server/src/main/java/seb44pre036/qna/question/entity/Question.java
@@ -43,12 +43,9 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answers = new ArrayList<>();
-
-//    public void addAnswers(Answer answer) {
-//        answers.add(answer);
-//        if(answer.getQuestion() != this){
-//            answer.addQuestion(this);
-//        }
-//    }
+    public void addAnswer(Answer answer) {
+      answers.add(answer);
+      //if(answer.getQuestion() != this){     answer.addQuestion(this); }
+  }
 
 }
