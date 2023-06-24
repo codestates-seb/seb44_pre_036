78cbe2a408a8541224808d7b50726cc0d0afa8e8
jasmine0714 <woooooooow22@gmail.com>
2023-06-24 21:54:25 +0900

    💄 회원가입 폼 좌측에 있는 가입 시 장점 list에 svg 이미지 적용

--- client/src/pages/SignUp/components/JoinBenefitList.tsx
+++ client/src/pages/SignUp/components/JoinBenefitList.tsx
@@ -1,17 +1,20 @@
-import { JoinBenefitWrapper, JoinBenefit } from '../style';
+import { JoinBenefitContainer } from '../style';
+import JoinBenefit from './JoinBenefit';
 
 function JoinBenefitList() {
   return (
-    <JoinBenefitWrapper>
-      <JoinBenefit>Get unstuck — ask a question</JoinBenefit>
-      <JoinBenefit>
-        Unlock new privileges like voting and commenting
-      </JoinBenefit>
-      <JoinBenefit>
-        Save your favorite questions, answers, watch tags, and more
-      </JoinBenefit>
-      <JoinBenefit>Earn reputation and badges</JoinBenefit>
-    </JoinBenefitWrapper>
+    <JoinBenefitContainer>
+      <JoinBenefit svgNumber={1} text={'Get unstuck — ask a question'} />
+      <JoinBenefit
+        svgNumber={2}
+        text={'Unlock new privileges like voting and commenting'}
+      />
+      <JoinBenefit
+        svgNumber={3}
+        text={'Save your favorite questions, answers, watch tags, and more'}
+      />
+      <JoinBenefit svgNumber={4} text={'Earn reputation and badges'} />
+    </JoinBenefitContainer>
   );
 }
 export default JoinBenefitList;
