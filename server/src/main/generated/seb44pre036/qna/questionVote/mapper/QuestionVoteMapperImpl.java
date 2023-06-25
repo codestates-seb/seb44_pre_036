6866201979a5306f336cf25c2c764cbdc0a8cac5
RedJunKi <bing8821@naver.com>
2023-06-26 01:06:18 +0900

    :bookmark: 배포용

---
+++ server/src/main/generated/seb44pre036/qna/questionVote/mapper/QuestionVoteMapperImpl.java
@@ -0,0 +1,13 @@
+package seb44pre036.qna.questionVote.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-26T01:04:07+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class QuestionVoteMapperImpl implements QuestionVoteMapper {
+}
