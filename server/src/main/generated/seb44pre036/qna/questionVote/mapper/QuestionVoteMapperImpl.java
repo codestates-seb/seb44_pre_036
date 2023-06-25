97ff6cde5b4c94f7c1f385196b284d03b91039c7
RedJunKi <bing8821@naver.com>
2023-06-26 00:56:54 +0900

    :bookmark: 배포용

--- server/src/main/generated/seb44pre036/qna/questionVote/mapper/QuestionVoteMapperImpl.java
+++ server/src/main/generated/seb44pre036/qna/questionVote/mapper/QuestionVoteMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-26T00:07:12+0900",
+    date = "2023-06-26T00:51:21+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
 )
 @Component
