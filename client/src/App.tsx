9eeafdb3c6a3e53041c5d70c577a30bdf856d3f6
seobi-97 <71924843+seobi-97@users.noreply.github.com>
2023-06-25 22:00:50 +0900

    🎨로그인 시, 비로그인 시 Router 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,14 @@ import { GlobalStyle, Layout } from './common/style';
 import GNB from './common/GNB/views/GNB';
 import useGetMe from './common/utils/customHook/useGetMe';
 import GSB from './common/GSB/views/GSB';
+import { useSelector } from 'react-redux';
+import { RootState } from './common/store/RootStore';
 
 function App() {
   // useGetMe()는 useQuery를 반환한다. refetch(서버에서 데이터를 다시 가져옴)를 사용하기 위해선 useQuery의 반환값을 사용해야 한다.
   const { refetch: refetchGetMe } = useGetMe();
-
+  const user = useSelector((state: RootState) => state.userInfo.memberId);
+  const isLogin = user !== 0;
   useEffect(() => {
     const autoLogin = async () => {
       const { data: userInfo } = await refetchGetMe();
@@ -26,7 +29,7 @@ function App() {
       <GlobalStyle />
       <GNB />
       <Layout>
-        <GSB />
+        {isLogin && <GSB />}
         <Router />
       </Layout>
     </>
--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/views/Home';
 import MyPage from './pages/MyPage/views/MyPage';
 import Board from './pages/Board/views/Board';
@@ -7,20 +7,41 @@ import Ask from './pages/Ask/views/Ask';
 import SignUp from './pages/SignUp/views/SignUp';
 import Detail from './pages/Detail/views/Detail';
 import Edit from './pages/Edit/views/Edit';
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { RootState } from './common/store/RootStore';
 
 function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/mypage/:id/:name/:tab" element={<MyPage />} />
-      <Route path="/board" element={<Board />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/ask/:id/:name" element={<Ask />} />
-      <Route path="/detail/:id" element={<Detail />} />
-      <Route path="/edit/:id" element={<Edit />} />
-    </Routes>
-  );
+  const [isLogin, setIsLogin] = useState(false);
+
+  const user = useSelector((state: RootState) => state.userInfo.memberId);
+  console.log(user);
+  useEffect(() => {
+    if (user !== 0) setIsLogin(true);
+    else setIsLogin(false);
+    console.log(isLogin);
+  }, [user]);
+  if (isLogin)
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage/:id/:name/:tab" element={<MyPage />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/ask/:id/:name" element={<Ask />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        {/* /login,signup으로의 접근 시 /로 리다이렉션 */}
+        <Route path="/login" element={<Navigate to="/" replace />} />
+        <Route path="/signup" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  else
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    );
 }
 
 export default Router;
