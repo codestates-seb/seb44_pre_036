785486da9d9d207911f0fd781045d6389c353d36
seobi-97 <71924843+seobi-97@users.noreply.github.com>
2023-06-27 00:40:40 +0900

    💄Answer Select, Vote

--- client/src/common/Answer/queries.ts
+++ client/src/common/Answer/queries.ts
@@ -7,6 +7,7 @@ const decrypt = useDecrypt();
 const accessToken = decrypt(localStorage.getItem('accessToken') || '');
 const header = { Authorization: `Bearer ${accessToken}` };
 const url = 'https://preprojectseb44036.kro.kr:8080/questions/answers/';
+
 //POST,GET,PATCH,DELETE Method
 const PostAnswer = async (data: Answerdata) => {
   const response = await axios.post(url, data, { headers: header });
@@ -22,6 +23,28 @@ const PatchAnswer = async (data: Patchdata) => {
 };
 const DeleteAnswer = async (id: string) => {
   const response = await axios.delete(`${url}/${id}`);
+  return response.data;
+};
+const SelectAnswer = async (id: string) => {
+  const response = await axios.patch(
+    `${url}/select`,
+    { answerId: id },
+    { headers: header },
+  );
+  return response.data;
+};
+const VoteAnswer = async (id: string, recommand: boolean) => {
+  const response = await axios.patch(
+    `${url}/vote`,
+    { answerId: id, recommand: recommand },
+    { headers: header },
+  );
+  return response.data;
+};
+const SortAnswer = async (id: string, sortedBy: string) => {
+  const response = await axios.get(`${url}/find`, {
+    params: { questionId: id, sortedBy: sortedBy },
+  });
   return response.data;
 };
 
@@ -63,6 +86,42 @@ export const PatchMutation = () => {
 
 export const DeleteMutation = (id: string) => {
   return useQuery(['answer', id], () => DeleteAnswer(id), {
+    onSuccess: async (data) => {
+      console.log(data);
+    },
+    onError: (error: AxiosError) => {
+      const { response } = error;
+      console.log(response);
+    },
+  });
+};
+
+export const SelectMutation = (id: string) => {
+  return useQuery(['answer', id], () => SelectAnswer(id), {
+    onSuccess: async (data) => {
+      console.log(data);
+    },
+    onError: (error: AxiosError) => {
+      const { response } = error;
+      console.log(response);
+    },
+  });
+};
+
+export const VoteMutation = (id: string, recommand: boolean) => {
+  return useQuery(['answer', id, recommand], () => VoteAnswer(id, recommand), {
+    onSuccess: async (data) => {
+      console.log(data);
+    },
+    onError: (error: AxiosError) => {
+      const { response } = error;
+      console.log(response);
+    },
+  });
+};
+
+export const SortMutation = (id: string, sortedBy: string) => {
+  return useQuery(['answer', id, sortedBy], () => SortAnswer(id, sortedBy), {
     onSuccess: async (data) => {
       console.log(data);
     },
